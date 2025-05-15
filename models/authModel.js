cdfcb3eae2f4dc16459154ba90115bc3a3a27654
AKSHAT GUPTA <gemini.akshatgupta@gmail.com>
2025-05-15 16:50:26 +0530

    Added password encryption using bcrypt

--- models/authModel.js
+++ models/authModel.js
@@ -18,9 +18,9 @@ const userSchema = new mongoose.Schema({
 
 // fire a function before doc saved to db
 userSchema.pre('save', async function(next) {
-    // const salt = await bcrypt.genSalt();
-    // this.password = await bcrypt.hash(this.password, salt);
-    console.log('user about to be created & saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    // console.log('user about to be created & saved', this);
     next();
 });
 
