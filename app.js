c7f15a036cceec33463e6449845b7137ba7a882d
AKSHAT GUPTA <gemini.akshatgupta@gmail.com>
2025-05-15 19:09:36 +0530

    Added cookies and cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config();
 
 const app = express();
@@ -8,12 +10,13 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
- 
+
 // database connection
-const dbURI = process.env.DB_URI; 
+const dbURI = process.env.DB_URI;
 mongoose.connect(dbURI)
   .then(() => app.listen(3000))
   .catch((err) => console.log('DB connection error:', err));
@@ -22,4 +25,17 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
- 
+
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+  // res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true });
+  res.send('You got the cookies!');
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});
