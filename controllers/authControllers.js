8bf62d9c0ccd5271c26f6d679ee3170ffc403d98
AKSHAT GUPTA <gemini.akshatgupta@gmail.com>
2025-05-15 12:17:31 +0530

    Added mongoose validation and error handling

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,27 @@
 const User = require('../models/authModel')
 
+const handleErrors = (err) => {
+    // console.log()
+    // console.log(err)
+    // console.log()
+    console.log(err.message, err.code)
+    let errors = { email: '', password: '' }
+
+    // duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'That email is already registered'
+        return errors
+    }
+    // validation errors
+    if (err.message.includes('User validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}   
+
+
 signup_get =  (req, res) => {
     res.render('signup')
 }
@@ -16,7 +38,11 @@ signup_post =  async (req, res) => {
         console.log(user)
         res.status(201).json(user)
     } catch (error) {
-        res.status(400).json({ error: error.message })
+        // res.send(error)
+        // console.log(error)
+        const errors = handleErrors(error)
+        // res.status(400).json({ error: error.message })
+        res.status(400).json(errors)
     }
 }
 login_post =  (req, res) => {
